4750471a157b1a52fd75a91ba611dcb56642b3fb
Timo Kreuzer <timo.kreuzer@reactos.org>
2022-12-14 09:35:35 +0200

    HACK [ADVAPI32_WINETEST] Improve code? Might not be needed.

--- modules/rostests/winetests/advapi32/eventlog.c
+++ modules/rostests/winetests/advapi32/eventlog.c
@@ -913,8 +913,8 @@ static void test_readwrite(void)
     /* Read all events from our created eventlog, one by one */
     handle = OpenEventLogA(NULL, eventlogname);
     ok(handle != NULL, "Failed to open Event Log, got %d\n", GetLastError());
-    i = 0;
-    for (;;)
+
+    for (i = 0; i < sizeof(read_write) / sizeof(read_write[0]); i++)
     {
         void *buf;
         DWORD read, needed;
@@ -1007,7 +1007,6 @@ static void test_readwrite(void)
            "Expected the closing DWORD to contain the length of the record\n");
 
         HeapFree(GetProcessHeap(), 0, buf);
-        i++;
     }
     CloseEventLog(handle);
 
